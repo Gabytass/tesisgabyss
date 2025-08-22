d5b1955b6401d0e562b3f97c099d88d192d403ed
Gabyssr <rojasgabriela633@gmail.com>
2025-08-21 23:08:06 -0500

    usar credenciales desde env

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 import os
 import json
 
-# Obtener credenciales desde la variable de entorno
+# Leer credenciales desde la variable de entorno
 cred_json = os.environ.get("FIREBASE_CREDENTIALS")
 if not cred_json:
     raise ValueError("No se encontró la variable de entorno FIREBASE_CREDENTIALS")
@@ -11,9 +11,11 @@ if not cred_json:
 cred_dict = json.loads(cred_json)
 cred = credentials.Certificate(cred_dict)
 
+# Inicializar Firebase si no está inicializado
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
+# Cliente de Firestore
 db = firestore.client()
 
 
